a66a7d07dc09dd252aeebf0ff49ceb834cfe8e43
Mozart Maia <mozartmaia89@gmail.com>
2023-01-22 19:04:27 -0300

    criacao de components

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+
 
 
 const app = express();
@@ -10,8 +12,11 @@ mongoose.connect('mongodb+srv://mozart:abcd1234@cluster0.hldiucm.mongodb.net/?re
     useUnifiedTopology: true,
 });
 
+app.use(cors());
+
 app.use(express.json()); //precisa vir antes das rotas para ser aplicada nelas
 
 app.use(routes); //rotas em arquivo separado
+
 
 app.listen(3333);
--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,125 +1,47 @@
-
-
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
 import './App.css';
 import './global.css';
 import './Sidebar.css';
 import './Main.css';
 
-
-
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
-  const [github_username, setUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);  
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-    
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, []);
   
+async function handleAddDev(data){
 
+  const response = await api.post('/devs', data);
+
+  setDevs([...devs, response.data]); //atualizar a lista de devs
+  // console.log(response);
+}
 
   return (
     <div id ="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor='github_username'>Usuario do github</label>
-            <input name="github_username" 
-            id="github_username" 
-            required 
-            value={github_username}
-            onChange={e => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-block">
-            <label htmlFor='techs'>Tecnologias</label>
-            <input name="techs" 
-            id="techs" 
-            required 
-            value={techs}
-            onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-          <div className='input-group'>
-            <div className="input-block">
-              <label htmlFor='latitude'>Latitude</label>
-              <input name="latitude" id="latitude" required value={latitude}/>
-            </div>
-          </div>
-          <div className="input-block">
-            <label htmlFor='longitude'>Longitude</label>
-            <input name="longitude" id="longitude" required value={longitude}/>
-          </div>
-
-          <button type='submit'>Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars.githubusercontent.com/u/37476677?v=4"></img>
-              <div className='user-info'>
-                <strong>Mozart Maia</strong>
-                <span>Python, Django, React</span>
-              </div>
-            </header>
-            <p>Desenvolvedor web lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum,</p>
-            <a href='https://github.com/MozartOMaia'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars.githubusercontent.com/u/37476677?v=4"></img>
-              <div className='user-info'>
-                <strong>Mozart Maia</strong>
-                <span>Python, Django, React</span>
-              </div>
-            </header>
-            <p>Desenvolvedor web lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum,</p>
-            <a href='https://github.com/MozartOMaia'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars.githubusercontent.com/u/37476677?v=4"></img>
-              <div className='user-info'>
-                <strong>Mozart Maia</strong>
-                <span>Python, Django, React</span>
-              </div>
-            </header>
-            <p>Desenvolvedor web lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum,</p>
-            <a href='https://github.com/MozartOMaia'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars.githubusercontent.com/u/37476677?v=4"></img>
-              <div className='user-info'>
-                <strong>Mozart Maia</strong>
-                <span>Python, Django, React</span>
-              </div>
-            </header>
-            <p>Desenvolvedor web lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum,</p>
-            <a href='https://github.com/MozartOMaia'>Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}   
         </ul>
-
       </main>      
     </div>    
   );
